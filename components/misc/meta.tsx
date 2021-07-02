f59d9ad0ee0657270fa0e47c547a4a3ea6b7ec3b
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-03 03:31:51 +0700

    add og:url and og:locale from router

--- components/misc/meta.tsx
+++ components/misc/meta.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { useRouter } from "next/router"
 import pjson from "../../package.json"
 
 type Props = {
@@ -8,6 +9,7 @@ type Props = {
 }
 
 const Meta = ({ title, meta, image }: Props) => {
+  const rounter = useRouter()
   return (
     <Head>
       {title && <title>{title}</title>}
@@ -18,11 +20,18 @@ const Meta = ({ title, meta, image }: Props) => {
       <meta name="title" content={title ?? pjson.name} />
       <meta name="description" content={pjson.description} />
       <meta name="version" content={pjson.version} />
+      <meta name="preview" content={rounter.isPreview ? "true" : "false"} />
 
       <meta property="og:site_name" content={title ?? pjson.name} />
       <meta property="og:title" content={title ?? pjson.name} />
+      <meta property="og:url" content={rounter.asPath} />
       <meta property="og:description" content={pjson.description} />
-      <meta property="og:locale" content="en-US" />
+      <meta property="og:locale" content={rounter.locale ?? "en-US"} />
+      {rounter.locales &&
+        rounter.locales.map((l) => {
+          return <meta key={l} property="og:locale:alternate" content={l} />
+        })}
+
       <meta property="og:type" content="website" />
       {image && <meta property="og:image" content={image} />}
 
