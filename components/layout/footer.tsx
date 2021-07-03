3d80afbfc755598e85a34be2704ca83ae95164fa
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-04 03:33:57 +0700

    perf(ui): add website version to footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,12 +1,14 @@
 import Link from "../misc/link"
 
+import pjson from "../../package.json"
+
 export const Footer = () => {
   return (
     <footer className="font-bold tracking-tight md:tracking-tighter leading-tight my-10 py-6 border-t">
       <span className="block text-small text-right">
         Made with 💖 using{" "}
         <Link href="https://github.com/kamontat/blog" name="Github" underline="always" window={true} /> and{" "}
-        <Link href="https://nextjs.org" name="Next.js" underline="always" window={true} />.
+        <Link href="https://nextjs.org" name="Next.js" underline="always" window={true} /> (v{pjson.version}).
       </span>
     </footer>
   )
