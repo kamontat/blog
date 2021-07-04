7494531a718ee648d4212ba79e98d4c1bda077ec
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-05 01:40:17 +0700

    chore: add deploy scripts and add CHANGELOG template

---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1000
+
+# generate by 2.3.2
+# link (https://github.com/Template-generator/script-genrating/tree/2.3.2)
+
+# set -x #DEBUG - Display commands and their arguments as they are executed.
+# set -v #VERBOSE - Display shell input lines as they are read.
+# set -n #EVALUATE - Check syntax of the script but don't execute.
+
+total_step=7
+
+echo "[1/$total_step] validate settings and repository:"
+if ! git diff --quiet --ignore-submodules; then
+  echo "your worktree is not clean"
+  git status --short --ignore-submodules
+
+  exit 1
+fi
+
+echo "[2/$total_step] list all tags that already exist:"
+git tag --column
+
+exit_keyword=""
+
+valid=false
+version=""
+while [[ "$valid" == "false" ]]; do
+  printf "[3/%s] Enter valid version (v0.0.0): " "$total_step"
+  read -r ans
+  if [[ $ans == "$exit_keyword" ]]; then
+    echo "bye-bye"
+    exit 0
+  fi
+
+  version="$ans"
+  if [[ "$version" =~ ^v ]]; then # must has prefix v
+    if ! git tag | grep -q "^$version$"; then
+      valid=true
+    fi
+  fi
+done
+test -z "$version" && exit 1
+
+echo "[4/$total_step] create release note"
+git-chglog --next-tag "$version" --output __posts/CHANGELOG.md
+
+echo "[5/$total_step] commit changes with release version message"
+git commit --allow-empty -am "chore(release): version $version"
+
+echo "[6/$total_step] create new git tag called '$version'"
+git tag "$version"
+
+echo "[7/$total_step] push all changes and tag to Github repository"
+git push && git push --tag
