b61dc3963a2b2d2ac381b52adac3533994fbe745
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-05 21:56:11 +0700

    feat(ui): update footer by adding changelog and tailwind

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,9 +6,12 @@ export const Footer = () => {
   return (
     <footer className="font-bold tracking-tight md:tracking-tighter leading-tight my-10 py-6 border-t">
       <span className="block text-small text-right">
-        Made with 💖 using{" "}
-        <Link href="https://github.com/kamontat/blog" name="Github" underline="always" window={true} /> and{" "}
-        <Link href="https://nextjs.org" name="Next.js" underline="always" window={true} /> (v{pjson.version}).
+        Made with 💖 using <Link href="https://nextjs.org" name="Next.js" underline="always" window={true} /> and{" "}
+        <Link href="https://tailwindcss.com" name="Tailwindcss" underline="always" window={true} />.
+      </span>
+      <span className="block text-small text-right">
+        Current version is `<Link href="/changelog" name={`v${pjson.version}`} underline="always" window={true} />` (
+        <Link href="https://github.com/kamontat/blog" name={`source`} underline="always" window={true} />)
       </span>
     </footer>
   )
